c91b954a23f1b312fd7549824ce35c875fa674dd
sammddltn <49808496+sammddltn@users.noreply.github.com>
2019-09-16 20:24:52 +1000

    implemented cinemachine camera

---
+++ Super Cubicle Adventure/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public GameObject cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.magnitude > 0.1)
+        {
+            cam.SetActive(true);
+        }
+        else
+        {
+            cam.SetActive(false);
+        }
+    }
+}
+
